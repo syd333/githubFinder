c15a32adc70226df00f151a61949a144d187cee7
syd333 <sydmiranda@icloud.com>
2022-07-28 13:48:15 -0400

    dummy navbar

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,10 +1,13 @@
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 function App() {
   return (
-    <div className="bg-purple-500">
-      <h1 className="text-xl">hello</h1>
-      <button className="btn">click</button>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main>content</main>
+      </div>
+    </Router>
   );
 }
 
