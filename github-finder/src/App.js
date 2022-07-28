bd0113fe74872a5123d155e29cafac258cbcde03
syd333 <sydmiranda@icloud.com>
2022-07-28 12:18:32 -0400

    tailwind + daisy ui setup

---
+++ github-finder/src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="bg-purple-500">
+      <h1 className="text-xl">hello</h1>
+      <button className="btn">click</button>
+    </div>
+  );
+}
+
+export default App;
